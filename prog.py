4db5aa6bb740530988accecd24df6914655da32e
4lex <94614466+cyberphar@users.noreply.github.com>
2023-08-07 12:59:37 +0200

    Update prog.py

--- prog.py
+++ prog.py
@@ -39,7 +39,7 @@ def get_hour(hour):
     return hours, min
 
 
-if sys.argv[0] == 1 && sys.argv[1] == "sun":
+if sys.argv[0] == 1 and sys.argv[1] == "sun":
     hour1 = sunrise_time.hour
     min1 = sunrise_time.minute
     
